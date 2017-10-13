8e08cdd33b42de92cd95b927ec9c86d51beca648
liushuai <909288037@qq.com>
2017-10-13 11:52:31 +0800

    完成集锦视频左右滑动

--- js/index.js
+++ js/index.js
@@ -22,7 +22,8 @@
 
     //myFormatTime:Format time
     pro.myFormatTime = function myFormatTime() {
-        var reg = /^(\d{4})(?:-|\/|\.|:)(\d{1,2})(?:-|\/|\.|:)(\d{1,2})(?: +)?(\d{1,2})?(?:-|\/|\.|:)?(\d{1,2})?(?:-|\/|\.|:)?(\d{1,2})?$/g, ary = [];
+        var reg = /^(\d{4})(?:-|\/|\.|:)(\d{1,2})(?:-|\/|\.|:)(\d{1,2})(?: +)?(\d{1,2})?(?:-|\/|\.|:)?(\d{1,2})?(?:-|\/|\.|:)?(\d{1,2})?$/g,
+            ary = [];
         this.replace(reg, function () {
             ary = ([].slice.call(arguments)).slice(1, 7);
         });
@@ -79,29 +80,80 @@
 
 //MATCH INFO
 var matchRender = (function () {
+    var $matchInfo = $('.matchInfo');
+
+    //  BIND EVENT
+    function bindEvent() {
+        var $bottom = $('.bottom'),
+            $bottomHome = $bottom.find('.home'),
+            $bottomAway = $bottom.find('.away');
+        //  获取本地信息
+        var support = localStorage.getItem('support');
+
+        if (support) {
+            support = JSON.parse(support);
+            console.log(support.istap);
+            if (support.istap) {
+                $bottom.attr('isTap', true);
+                support.type == 1 ? $bottomHome.addClass("bg") : $bottomAway.addClass("bg");
+            }
+        }
+        $matchInfo.tap(function (e) {
+            var tar = e.target,
+                tarTag = tar.tagName,
+                tarP = tar.parentNode,
+                $tar = $(tar),
+                $tarP = $tar.parent(),
+                tarInn = $tar.html();
+            if (tarTag === 'SPAN' && tarP.className === "bottom") {
+                if ($bottom.attr('isTap') === "true") {
+                    return;
+                }
+                if (tar.className === 'type') {
+                    return;
+                }
+                $tar.html(parseFloat(tarInn) + 1).addClass("bg");
+
+                //    发送给服务器支持的谁
+                $.ajax({
+                    url: 'http://matchweb.sports.qq.com/kbs/teamSupport?' +
+                    'mid=100000%3A1471455&type=' + $tar.attr("type"),
+                    dataType: 'jsonp',
+                    jsonpCallback: 'support'
+                });
+
+                //只能点击一次
+                $bottom.attr("isTap", true);
+                localStorage.setItem('support', JSON.stringify({'istap': true, "type": $tar.attr("type")}));
+            }
+        })
+    }
+
+    //  BIND HTML
     function bindHTML(matchinfo) {
         var obj = {
-            matchinfo : [matchinfo]
+            matchinfo: [matchinfo]
         }
         //  调用模版引擎
-        var resultStr = template("template",obj);
+        var resultStr = template("template", obj);
         $('.matchInfo').html(resultStr);
         //  控制进度条
         window.setTimeout(function () {
             var leftNum = parseFloat(matchinfo.leftSupport),
                 rightNum = parseFloat(matchinfo.rightSupport);
-            $('.middle span').css('width',(leftNum/(leftNum+rightNum))*100+'%');
-        },500);
+            $('.middle span').css('width', (leftNum / (leftNum + rightNum)) * 100 + '%');
+        }, 500);
     }
+
     return {
         init: function () {
-        //    get data
-            $.ajax ({
+            //    get data
+            $.ajax({
                 url: "http://matchweb.sports.qq.com/html/matchDetail?" +
                 "mid=100000%3A1471434&_=1507727623690&callback=matchDetailCallback",
-                dataType:"jsonp",
+                dataType: "jsonp",
                 success: function (data) {
-                    if (data&&data[0] === 0) {
+                    if (data && data[0] === 0) {
                         data = data[1];
                         console.log(data);
                         var matchinfo = data["matchInfo"];
@@ -110,8 +162,11 @@ var matchRender = (function () {
                         matchinfo["startTime2"] = matchinfo['startTime'].myFormatTime('{1}月{2}日{3}:{4}');
                         console.log(matchinfo);
 
-                    //    bind HTML
+                        //    bind HTML
                         bindHTML(matchinfo);
+
+                        //    bind event
+                        bindEvent();
                     }
                 }
             });
@@ -119,3 +174,55 @@ var matchRender = (function () {
     }
 })();
 matchRender.init();
+
+//MATCH LIST
+var matchListRender = (function () {
+    function bindHtml(matchList) {
+        var obj = {
+            matchList: matchList
+        };
+        $('.clear').html(template("template2", obj)).css({width: $(".clear").children("li").length * 2.4 + "rem"});
+        $(".clear").children("li").eq(0).addClass("bg");
+        $('.clear').children("li").on("click",function () {
+            $(this).addClass("bg").siblings().removeClass("bg");
+            var playUrl = $(this).attr("playUrl");
+            console.log($("video").attr("src",playUrl));
+        });
+        //    实现局部滚动
+        new IScroll('.matchList', {
+            scrollX: true,
+            scrollY: false,
+            click: true
+        })
+
+    }
+
+    return {
+        init: function () {
+            $.ajax({
+                type: 'get',
+                url: 'http://matchweb.sports.qq.com/html/matchStatV37?mid=100000%3A1471455',
+                dataType: 'jsonp',
+                success: function (data) {
+                    if (data && data[0] == 0) {
+                        data = data[1].stats;
+                        console.log(data);
+                        var matchList = null;
+                        $.each(data, function (index, item) {
+                            console.log(item);
+                            if (item.type == 9) {
+                                matchList = item.list;
+                                $.each(matchList, function (index, item) {
+                                    item.duration = item.duration.slice(3);
+                                });
+                                return false;
+                            }
+                        });
+                        bindHtml(matchList);
+                    }
+                }
+            })
+        }
+    }
+})();
+matchListRender.init();
